ae00ad3c542200cab6203c6bd0c171bf42bfadc1
viniciustmota <motaviny140@gmail.com>
2025-06-04 12:33:23 -0300

    feat: criado o MyContext

---
+++ src/Api.Data/Context/MyContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        // pesquisar no gemini pq tem que fazer assim
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

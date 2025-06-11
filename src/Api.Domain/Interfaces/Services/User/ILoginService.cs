a3b919f605af18edaf97c2089129084e40b1c1bf
eduardo-zucco <eduardozucco89@gmail.com>
2025-06-11 07:44:47 -0300

    feat: implement jwt

---
+++ src/Api.Domain/Interfaces/Services/User/ILoginService.cs
@@ -0,0 +1,9 @@
+using Api.Domain.Entities;
+
+namespace Api.Domain.Interfaces.Services.User
+{
+    public interface ILoginService
+    {
+        Task<object> FindByLogin(UserEntity user);
+    }
+}
---
+++ src/Api.Domain/Repository/IUserRepository.cs
@@ -0,0 +1,10 @@
+using Api.Domain.Entities;
+using Api.Domain.Interfaces;
+
+namespace Api.Domain.Repository
+{
+    public interface IUserRepository : IRepository<UserEntity>
+    {
+        Task<UserEntity> FindByLogin(string email);
+    }
+}

1c65c702039b71b8e93d2b8651965f05ea774269
viniciustmota <motaviny140@gmail.com>
2025-06-11 10:08:06 -0300

    feat: implement Login and LoginDto

---
+++ src/Api.Application/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+using System.Net;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+namespace Api.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<object> Login([FromBody] UserEntity userEntity, [FromServices] ILoginService service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (userEntity == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var result = await service.FindByLogin(userEntity);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
